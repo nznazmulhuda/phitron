10d19637d37a5d5d361bee7728a1fbe9d5412ee0
Nazmul Huda <nznazmulhuda04@gmail.com>
2025-04-02 19:17:18 +0600

    first module - phitron (PRACTICE)

---
+++ week - 0/module - 0/test_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int a;
+
+  // enter the value of a
+  printf("Enter the value of a: ");
+  scanf("%d", &a);
+
+  // print the value of a
+  printf("The value of 'a' is equal to: %d", a);
+
+  return 0;
+}
